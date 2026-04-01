8d9e34d8a2210c7daa058158f807e23e8840df77
valerosoo <fran.e.negri@gmail.com>
2026-03-31 23:51:19 -0300

    Elimine los assets del ramen, me complicaban el flip.h, lo cambie por el sprite actual. ya hice las boxes a todos los personajes. todavia no está hecho el daño, vida, attackHitbox funcional (no se activa en el frame que debe para hacer daño)

--- beat-em-up/scripts/enemigo_1.gd
+++ beat-em-up/scripts/enemigo_1.gd
@@ -6,23 +6,25 @@ enum Estado {IDLE, CHASE, ATTACK}
 @onready var attack_area = get_node("AttackArea")
 
 @export var speed = 120
-@export var wait_time = 2
-@export var distancia_para_atacar = 100
+@export var distancia_para_atacar = 140
+@export var nombre_animacion_atacar = "combo"
 
 var estado = Estado.IDLE
-var posicion_inicial
-var objetivo_posicion
-var timer = 0
 var atacando = false
 var attack_offset
 var player
+var player_hurtBox
+var distancia
 
 func _ready():
-	posicion_inicial = global_position
 	attack_offset = attack_area.position.x
 	player = get_tree().get_first_node_in_group("jugador")
+	player_hurtBox = player.get_node("HurtBox")
 	
 func _physics_process(delta):
+	
+	distancia = global_position.distance_to(player_hurtBox.global_position)
+	
 	girar_sprite()
 	
 	match estado:
@@ -55,15 +57,13 @@ func perseguir(delta):
 
 	$AnimatedSprite2D.play("walk")
 	
-	var distancia = global_position.distance_to(player.global_position)
-	
 	if distancia < distancia_para_atacar:
 		estado = Estado.ATTACK
 
 func atacar():
 	velocity = Vector2.ZERO
 	
-	var distancia = global_position.distance_to(player.global_position)
+	
 	if distancia > distancia_para_atacar:
 		atacando = false
 		estado = Estado.CHASE
@@ -72,15 +72,13 @@ func atacar():
 	if !atacando:
 		atacando = true
 		$AnimatedSprite2D.frame = 0
-		$AnimatedSprite2D.play("combo")
+		$AnimatedSprite2D.play(nombre_animacion_atacar)
 
 func _on_animated_sprite_2d_animation_finished():
 
-	if $AnimatedSprite2D.animation == "combo":
+	if $AnimatedSprite2D.animation == nombre_animacion_atacar:
 		atacando = false
 		
-		var distancia = global_position.distance_to(player.global_position)
-
 		if distancia < distancia_para_atacar:
 			estado = Estado.ATTACK
 			
@@ -92,3 +90,7 @@ func girar_sprite():
 	elif velocity.x < 0:
 		$AnimatedSprite2D.flip_h = true
 		$AttackArea.position.x = -attack_offset
+		
+func _on_radar_body_exited(body: Node2D) -> void:
+	if body.is_in_group("jugador"):
+		estado = Estado.IDLE
